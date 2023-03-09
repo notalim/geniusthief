8f0651177d7c2d7331b88480de317a3ee376e806
notalim <alimneedstowork@gmail.com>
2023-03-09 15:07:49 -0500

    added readme, fixed the script

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# geniusthief
- Python Script meant to scrape lyrics data from Genius using Genius API,
+# geniusthief.py
+Python Script meant to scrape lyrics data by artist from Genius using Genius API. Inspired by [this 2019 article](https://medium.com/analytics-vidhya/how-to-scrape-song-lyrics-a-gentle-python-tutorial-5b1d4ab351d2) initally, but made to work with in 2023.
+
+Feel free to collaborate and work on this deeper, as I won't probably continue work on this beyond this point.
+
+Before using make sure to install `requests` and `bs4` with:
+```
+pip install requests
+pip install bs4
+```
+or 
+```
+pip3 install requests
+pip3 install bs4
+```
+
+## Instructions on how to use:
+
+1. Get a Genius API Token [here](https://medium.com/analytics-vidhya/how-to-scrape-song-lyrics-a-gentle-python-tutorial-5b1d4ab351d2).
+
+2. Insert your token in the `TOKEN` variable.
+
+3. IMPORTANT! Make sure that the `div`'s class name in `lyrics_container` variable inside of the `scrape_song_lyrics(url)` function is correct. You can do that by inspecting the element on the page of the artist you need.
+
+4. Insert the artist name in the end of the file. 
+
+5. Create a folder `lyrics` inside of the folder with the script before running it. You can easily do it by running this command in the terminal:
+
+```
+mkdir ./lyrics 
+```
+
+### That's it! You can use your lyrics text file for your purposes.
+
+Feel free to give any inquiries and contact me @ https://notalim.com/
---
+++ genius_thief.py
@@ -0,0 +1,91 @@
+# Make HTTP requests
+import requests
+# Scrape data from an HTML document
+from bs4 import BeautifulSoup
+# I/O
+import os
+# Search and manipulate strings
+import re
+
+GENIUS_API_TOKEN='insert your token here'
+
+# Get artist object from Genius API
+def request_artist_info(artist_name, page):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + GENIUS_API_TOKEN}
+    search_url = base_url + '/search?per_page=10&page=' + str(page)
+    data = {'q': artist_name}
+    response = requests.get(search_url, data=data, headers=headers)
+    return response
+# Get Genius.com song url's from artist object
+def request_song_url(artist_name, song_cap):
+    page = 1
+    songs = []
+    
+    while True:
+        response = request_artist_info(artist_name, page)
+        json = response.json()
+        # Collect up to song_cap song objects from artist
+        song_info = []
+        for hit in json['response']['hits']:
+            if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
+                song_info.append(hit)
+    
+        # Collect song URL's from song objects
+        for song in song_info:
+            if (len(songs) < song_cap):
+                url = song['result']['url']
+                songs.append(url)
+            
+        if (len(songs) == song_cap):
+            break
+        else:
+            page += 1
+        
+    print('Found {} songs by {}'.format(len(songs), artist_name))
+    return songs
+    
+# Scrape lyrics from a Genius.com song URL
+# def scrape_song_lyrics(url):
+#     page = requests.get(url)
+#     html = BeautifulSoup(page.text, 'html.parser')
+#     lyrics = html.find('div', class_ = "Lyrics__Container-sc-1ynbvzw-6 YYrds").get_text()
+#     #remove identifiers like chorus, verse, etc
+#     lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
+#     #remove empty lines
+#     lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])         
+#     return lyrics
+
+def scrape_song_lyrics(url):
+    page = requests.get(url)
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics_container = html.find('div', class_ = "Lyrics__Container-sc-1ynbvzw-6 YYrds")
+    # ADD a "\n" before every <br/>
+    for br in lyrics_container.find_all("br"):
+        br.replace_with("\n")
+    lyrics_lines = lyrics_container.get_text().split("\n")
+    #remove identifiers like chorus, verse, etc
+    lyrics_lines = [re.sub(r'[\(\[].*?[\)\]]', '', line) for line in lyrics_lines]
+    #remove empty lines
+    lyrics_lines = [(line.strip()) for line in lyrics_lines if line.strip()]
+
+    lyrics = os.linesep.join(lyrics_lines)
+    return lyrics
+
+# DEMO
+#print(scrape_song_lyrics('inser a song lyric'))
+
+def write_lyrics_to_file(artist_name, song_count):
+    f = open('lyrics/' + artist_name.lower() + '.txt', 'w', encoding="utf-8")
+    urls = request_song_url(artist_name, song_count)
+    for url in urls:
+        lyrics = scrape_song_lyrics(url)
+        f.write(lyrics)
+        f.write('\n') # Add a newline character after each song's lyrics
+    f.close()
+    num_lines = sum(1 for line in open('lyrics/' + artist_name.lower() + '.txt', 'rb'))
+    print('Wrote {} lines to file from {} songs'.format(num_lines, song_count))
+  
+# DEMO  
+write_lyrics_to_file('artist name', 100)
+
